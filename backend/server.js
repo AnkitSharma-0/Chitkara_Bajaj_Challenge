17b7311a5b52aaf135c247f740ec8b3cb040acd4
Ankit <sharmankit823@gmail.com>
2026-06-24 20:20:22 +0530

    Fixed root route

--- backend/server.js
+++ backend/server.js
@@ -7,13 +7,22 @@ const hierarchyRoutes = require("./routes/hierarchyRoutes");
 
 const app = express();
 
+// Middlewares
 app.use(cors());
 app.use(express.json());
 
+// Root Route
+app.get("/", (req, res) => {
+  res.send("Chitkara Bajaj Challenge Backend Running");
+});
+
+// API Routes
 app.use("/api", hierarchyRoutes);
 
+// Server Port
 const PORT = process.env.PORT || 3000;
 
+// Start Server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
